61b4044a9d66e262f9c23c254960072b08432157
Tomaž Dobravec <tomaz.dobravec@fri.uni-lj.si>
2016-10-28 09:29:32 +0200

    - .

--- ALGator/settings.py
+++ ALGator/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'mod_wsgi.server',
     'login',
     'problems',
     'cpanel',
